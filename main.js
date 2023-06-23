95396b2ba28a2aaf42a38ce68f3b317f2d29e841
midoAndrey <93534238+midoAndrey@users.noreply.github.com>
2023-06-23 15:19:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const wrapper = document.querySelector(".wrapper"),
+let wrapper = document.querySelector(".wrapper"),
     qrInput = wrapper.querySelector(".form input"),
     generateBtn = wrapper.querySelector(".form button"),
     qrImg = wrapper.querySelector(".qr-code img");
